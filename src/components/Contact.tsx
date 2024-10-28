c1b95bf7ebd694744111126ede3587664296cc58
Jujock004 <julien.joecker@gmail.com>
2024-10-28 15:08:47 +0100

    "add of a form with a state to hide it"

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
+import ContactForm from "./ContactForm";
+
 function Contact() {
+	const [isHidden, setHidden] = useState(true);
 	return (
 		<section id="Contact">
 			<h2>Contact.</h2>
+			<button type="button" onClick={() => {setHidden(false)}}>Contact-me</button>
+			<ContactForm isHidden={isHidden} setHidden={setHidden}/>
 		</section>
 	);
 }
---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,35 @@
+interface ContactFormProps {
+    isHidden: boolean;
+    setHidden: (hidden: boolean) => void;
+}
+
+function ContactForm({isHidden, setHidden}:ContactFormProps)  {
+    return(
+        <>
+            <form className={isHidden ? "hidden":""}>
+                <ul>
+                    <li>
+                        <label>Nom :&nbsp;
+                            <input type="text" id="name" name="user_name" />
+                        </label>
+                    </li>
+                    <li>
+                        <label>E-mail :&nbsp;
+                            <input type="email" id="mail" name="user_mail" />
+                        </label>
+                    </li>
+                    <li>
+                        <label>Message :&nbsp;
+                            <textarea id="msg" name="user_message" />
+                        </label>
+                    </li>
+                </ul>
+                <button onClick={() => setHidden(true)}>
+                    Soumettre
+                </button>
+            </form>
+        </>
+    )
+}
+
+export default ContactForm;
