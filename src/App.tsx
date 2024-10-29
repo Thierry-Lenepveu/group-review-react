53333b32c3ca5163b082a3a574f515425d2b3743
Jujock004 <julien.joecker@gmail.com>
2024-10-29 12:27:18 +0100

    added css for about

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import "./css/Services.css";
 import './css/Work.css'
 import "./css/Contact.css"
 import "./css/Footer.css";
+import "./css/About.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
